b8a536b6de12f88f67bee727f1aa1482f950a374
flow4u <laulencee1@gmail.com>
2026-09-08 10:42:16 +0700

    Character Creation.

---
+++ Lab04/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Student ID : 1690703135
+ * Name       : Lab04
+ * Section    : 129C
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+namespace Lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+------------------------------+");
+            Console.WriteLine("|      CHARACTER CREATION       |");
+            Console.WriteLine("+------------------------------+");
+            Console.Write("Name your character: ");
+            string characterName = Console.ReadLine();
+            Console.Write("Choose a class (1-3): ");
+            bool classOk = int.TryParse(Console.ReadLine(), out int characterNum);
+            Console.Write("Starting luck (0.0-10.0): ");
+            bool luckOk = double.TryParse(Console.ReadLine(), out double luck);
+            Console.WriteLine($"{characterName} the Class-{characterNum} adventurer enters the dungeon. Luck: {luck}");
+        }
+    }
+}
